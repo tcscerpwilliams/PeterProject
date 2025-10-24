2321951a93a2ae7e2dcad5ea473914a70666c357
tcscerpwilliams <tcscerpwilliams@gmail.com>
2025-10-23 18:00:40 -0700

    WORKING
    WE DID STUFF

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    public Rigidbody body;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +14,35 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var direction = new Vector3(-1, 0, 0);
+        int x = 0;
+        int z = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            z = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            z = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            x = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            x = -1;
+        }
+
+
+        var direction = new Vector3(x, 0, z);
        
-        transform.position += new Vector3(-1, 0, 0);
+
+        transform.position += direction*Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            body.AddForce(Vector3.up * 200);
+        }
 
     }
 }
